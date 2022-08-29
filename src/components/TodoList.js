7d2516861f21abb57e31c682c51b6d58c8110ec5
Negar Saleh <ngrslh76@gmail.com>
2022-08-30 00:18:00 +0430

    add filteredTodos to TodoList

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,20 @@
 import React from "react";
+import Todo from "./Todo";
 
-const TodoList = () => {
+const TodoList = ({todos , setTodos , filteredTodos}) => {
     return (
         <div className="todo-container">
-            <ul className="todo-list"></ul>
+            <ul className="todo-list">
+                {filteredTodos.map( (todo) => (
+                    <Todo 
+                    key={todo.id} 
+                    text={todo.text} 
+                    todos={todos} 
+                    setTodos={setTodos}
+                    todo={todo}
+                    />
+                ))}
+            </ul>
         </div>
     );
 };
